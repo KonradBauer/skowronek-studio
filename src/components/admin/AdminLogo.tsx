da6815b7c69c90d1c408ecc4fd05eb96aeb21916
Konrad Bauer <konrad.bauer@remoterebels.com>
2026-04-14 15:03:10 +0200

    fix logo

--- src/components/admin/AdminLogo.tsx
+++ src/components/admin/AdminLogo.tsx
@@ -1,6 +1,7 @@
 export default function AdminLogo() {
   return (
     <div style={{ background: '#ffffff', borderRadius: '8px', padding: '8px 16px', display: 'inline-flex' }}>
+      {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
         src="/logo.png"
         alt="Skowronek Studio"
